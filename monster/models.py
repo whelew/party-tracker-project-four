8a08f20d0510aa4bcf8d2f6f6bb81e52cc098f7a
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-01-02 14:51:50 +0000

    Created Monster model inside monster.models.py

--- monster/models.py
+++ monster/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Monster(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    challenge_rating = models.IntegerField(default=0)
+    health = models.IntegerField(default=0)
+    armour = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    type = models.CharField(max_length=50)
+    size = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
