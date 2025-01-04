36e0d27aae0040a51e8b93734a31cdd26e6b0732
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-01-04 15:49:29 +0000

    Updated party_trackerr/urls.py monster/

--- party_tracker/urls.py
+++ party_tracker/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("monster.urls"), name="monster-urls"),
+    path("monster/", include("monster.urls"), name="monster-urls"),
     path('admin/', admin.site.urls),
 ]
