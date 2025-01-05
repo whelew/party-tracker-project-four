7a19976462d217c894cae5c1f83e851b09a09d5d
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-01-05 19:44:07 +0000

    Created form.py in campaigns, new create_campaign.html template, created new view to allow user to create campaign when logged in

--- campaign/views.py
+++ campaign/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Campaign, Character
+from .forms import CampaignForm
 
 # Create your views here.
 @login_required
@@ -24,3 +25,17 @@ def campaign_list(request):
         request, "campaign/campaign.html",
         {"current_campaign": current_campaign},
         )
+
+@login_required
+def create_campaign(request):
+    if request.method == 'POST':
+        form = CampaignForm(request.POST)
+        if form.is_valid():
+            campaign = form.save(commit=False)
+            campaign.user =request.user
+            campaign.save
+            return redirect('campaign_list')
+    else:
+        form = CampaignForm
+    
+    return render(request, 'campaign/create_campaign.html', {'form': form})
