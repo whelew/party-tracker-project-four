8297ea66b8a30a77eea3dfc20633d518e3fcfede
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-01-05 23:49:26 +0000

    Create CharacterForm in forms.py

--- campaign/forms.py
+++ campaign/forms.py
@@ -1,7 +1,24 @@
 from django import forms
-from .models import Campaign
+from .models import Campaign, Character
 
 class CampaignForm(forms.ModelForm):
     class Meta:
         model = Campaign
         fields = ['name', 'description']
+
+class CharacterForm(forms.ModelForm):
+
+    class Meta:
+        model = Character
+        fields = [
+            'name', 'character_class', 'health', 'strength', 'dexterity', 
+            'constitution', 'intelligence', 'wisdom', 'charisma', 'campaign',
+        ]
+    
+    def __init__(self, *args, **kwargs):
+        # Capture user instance passed from the view
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        # Filter campaigns only belonging to loggedin user
+        if user:
+            self.fields['campaign'].queryset = Campaign.objects.filter(user=user)
