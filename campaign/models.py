088811727963e16758d22d2e690b75a4cc9551dc
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-01-05 17:50:04 +0000

    Created Campaign model

--- campaign/models.py
+++ campaign/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Campaign(models.model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='campaigns')
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self
