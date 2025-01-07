b8785eaa601682e2463a4f1385520a11b367b77b
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-01-07 13:20:32 +0000

    Adjusted syntax in new forms.py

--- item/forms.py
+++ item/forms.py
@@ -2,9 +2,7 @@ from django import forms
 from .models import Item, Inventory, InventoryItem
 
 class AddItemForm(forms.Form):
-    """
-    Allow user to select an item and quantity of item.
-    """
+    # Allow user to select an item and quantity of item.
     item = forms.ModelChoiceField(queryset=Item.objects.all(), label="Select Item")
     quantity = forms.ModelChoiceField(min_value=1, label='Quantity')
 
