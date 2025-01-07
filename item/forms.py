725d08d62b27306ceb2abd988b5740f20caba2f4
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-01-07 15:08:24 +0000

    Updated form, quantity is  is now IntField, replaced ModelChoiceField

--- item/forms.py
+++ item/forms.py
@@ -4,5 +4,5 @@ from .models import Item, Inventory, InventoryItem
 class AddItemForm(forms.Form):
     # Allow user to select an item and quantity of item.
     item = forms.ModelChoiceField(queryset=Item.objects.all(), label="Select Item")
-    quantity = forms.ModelChoiceField(min_value=1, label='Quantity')
+    quantity = forms.IntegerField(min_value=1, label='Quantity')
 
