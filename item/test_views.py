2f9a1e860983f8f91761055b31aa898f738778d3
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-01-12 16:13:22 +0000

    Created AddItemToInventoryTest, in item tests_views.py

--- item/test_views.py
+++ item/test_views.py
@@ -3,13 +3,14 @@ from django.urls import reverse
 from django.test import TestCase
 from .forms import AddItemForm
 from .models import Item, Inventory, InventoryItem
+from campaign.models import Campaign, Character
 
 
 class ItemListTest(TestCase):
     def setUp(self):
-        Item.objects.create(name="Rope", description="50m of Rope.")
-        Item.objects.create(name="Fishing Rod", description="A device used for fishing.")
-        Item.objects.create(name="Torch", description="A source of light when in the darkest of dungeons.")
+        Item.objects.create(name='Rope', description='50m of Rope.')
+        Item.objects.create(name='Fishing Rod', description='A device used for fishing.')
+        Item.objects.create(name='Torch', description='A source of light when in the darkest of dungeons.')
        
 
     def test_item_library(self):
@@ -22,3 +23,34 @@ class ItemListTest(TestCase):
         self.assertContains(response, 'Rope')
         self.assertContains(response, 'Fishing Rod')
         self.assertContains(response, 'Torch')
+
+class AddItemToInventory(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='password123', email='test@test.com')
+        self.campaign = Campaign.objects.create(name='Testcampaign', description='This is a test', user=self.user)
+        self.character = Character.objects.create(name='Gandalf', campaign=self.campaign)
+        self.inventory = Inventory.objects.get(character=self.character)
+        self.item = Item.objects.create(name='Rope', description='50m of Rope.')
+
+    def test_add_item_get(self):
+        self.client.login(username='test', password='password123')
+        url = reverse('add_item_to_inventory', kwargs={'character_id': self.character.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,'inventory/character_inventory.html')
+        self.assertIn('form', response.context)
+        self.assertIn('inventory', response.context)
+        self.assertIn('inventory_items', response.context)
+
+    def test_add_item_post(self):
+        login_success = self.client.login(username='test', password='password123')
+        self.assertTrue(login_success)
+        url = reverse('add_item_to_inventory', kwargs={'character_id': self.character.id})
+        form_data = {
+            'item':self.item.id,
+            'quantity': 2
+        }
+        response = self.client.post(url, form_data)
+        inventory_item = InventoryItem.objects.get(inventory=self.inventory, item=self.item)
+        self.assertEqual(inventory_item.quantity, 2)
+        self.assertRedirects(response, url)
