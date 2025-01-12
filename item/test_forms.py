a470b5d0ec629c1ed368be3e3b04f6f5b2f81fb3
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-01-12 21:20:44 +0000

    Updated AddItemForm test tto check for invalid fields

--- item/test_forms.py
+++ item/test_forms.py
@@ -6,14 +6,32 @@ class TestAddItemForm(TestCase):
 
     def setUp(self):
         Item.objects.get_or_create(name='Crossbow')
+        self.item_instance = Item.objects.first()
 
     def test_form_is_valid(self):
         """ Test for all fields"""
 
-        item_instance = Item.objects.first()
-
         form = AddItemForm({
-            'item': item_instance.id,
+            'item': self.item_instance.id,
             'quantity': '1',
         })
-        self.assertTrue(form.is_valid(), msg="Form is not valid")
+        self.assertTrue(form.is_valid(), msg="Form is valid")
+
+    def test_form_invalid_item(self):
+        """Test empty item field"""
+
+        form = AddItemForm({
+            'item': '',
+            'quantity': 1
+        })
+        self.assertFalse(form.is_valid(), msg='Form is not valid')
+
+    def test_form_invalid_quantity(self):
+        """Test empty quantity, or below 0"""
+
+        for validator in ['', 0]:
+            form = AddItemForm({
+                'item': self.item_instance.id,
+                'quantity': validator
+            })
+        self.assertFalse(form.is_valid(), msg=f'Form is not valid if quantity value is {validator}')
